3f8a68f2f00636e76b601aabcc7b5f5d3b1b7500
Dylan <15155562278@qq.com>
2023-08-18 20:57:58 +0800

    2023.8.18.2

--- test/mian.cpp
+++ test/mian.cpp
@@ -3,13 +3,17 @@
 // 函数接受一个数组和数组的大小作为参数，并返回数组中元素的个数
 template <typename T, size_t N>
 size_t countElements(const T (&arr)[N]) {
-    return N;
+    if(N == 0)
+    return *arr;
+    else{
+
+    }
 }
 
 int main() {
-    int myArray[] = {1, 2, 3, 4, 5};
+    int myArray[] = {1};
     size_t elementCount = countElements(myArray);
-
+    
     std::cout << "数组中元素的个数: " << elementCount << std::endl;
 
     return 0;
